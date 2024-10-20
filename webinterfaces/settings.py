4e28c57da1abeb35db6333587fb53960f541fb19
Luis-2102 <143152087+Luis-2102@users.noreply.github.com>
2024-10-20 10:47:45 -0500

    Update settings.py

--- webinterfaces/settings.py
+++ webinterfaces/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #media
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
